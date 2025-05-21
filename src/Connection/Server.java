e1a5a43b7831500aefebf3bd6efe6a9e943b982b
mbsurya19122004 <mbsurya19122004@gmail.com>
2025-05-21 10:27:41 +0530

    server file created 2

--- src/Connection/Server.java
+++ src/Connection/Server.java
@@ -1,4 +1,37 @@
 package Connection;
 
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Server {
+    int port;
+    ServerSocket Server;
+
+
+
+    public Server(){
+        try{
+            this.Server = new ServerSocket(0);
+            this.port= Server.getLocalPort();
+            System.out.println("Server started : " + this.port);
+        } catch (IOException e) {
+            System.out.println("error at Server constr");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void letConnect(){
+        try{
+            Socket client = this.Server.accept();
+            String clientIp = client.getInetAddress().toString();
+            int clientPort = client.getLocalPort();
+            System.out.println("Client connected : " + clientIp + clientPort);
+        } catch (IOException e) {
+            System.out.println("error at Server letconnect");
+            throw new RuntimeException(e);
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+import Connection.Server;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        init();
+    }
+
+    public static void init(){
+            Server server = new Server();
+            //server.letConnect();
+
+            Scanner sc = new Scanner(System.in);
     }
 }
