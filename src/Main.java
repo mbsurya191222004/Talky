ae9e723c93cde4e3aa442669c500f1910a04d931
lakshya <lakshyarajputr@gmail.com>
2025-05-22 11:12:24 +0530

    added client socket connection

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,40 @@ import Connection.Server;
 
 public class Main {
 
-    Socket client= new Socket();
-    Scanner sc = new Scanner(System.in);
+
+
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         init();
+
+        System.out.println("wanna connect?(y/n)");
+        String var = sc.nextLine();
+        if(var.equals("y")){
+            System.out.println("IP>>>");
+            String ip = sc.nextLine();
+            System.out.println("Port>>>");
+            int port = sc.nextInt();
+            connect(ip,port);
+        }
+
+
     }
 
     public static void init(){
             Server server = new Server();
-            server.letConnect();
+            //server.letConnect();
 
-            Scanner sc = new Scanner(System.in);
+
+
     }
 
-    public static void connect(String ip,int port,Socket socket) {
-        try{
-            socket.connect(new InetSocketAddress(ip,port));
+    public static void connect(String ip,int port) {
+
+        try(Socket socket= new Socket()){
+            socket.connect(new InetSocketAddress(ip,port),1000);
+            System.out.println("connected to : "+ socket.getRemoteSocketAddress() + socket.getPort());
+
         } catch (IOException e) {
             System.out.println("error in  connect");
             throw new RuntimeException(e);
