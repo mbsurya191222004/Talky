cc3464939dbc30453f3b7ae0f20f5778924fe732
mbsurya19122004 <mbsurya19122004@gmail.com>
2025-05-23 03:03:28 +0530

    added scanner object after messing up code twice messed up

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,33 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         init();
 
-        System.out.println("wanna connect?(y/n)");
-        String var = sc.nextLine();
-        if(var.equals("y")){
-            System.out.println("IP>>>");
-            String ip = sc.nextLine();
-            System.out.println("Port>>>");
-            int port = sc.nextInt();
-            connect(ip,port);
-        }
+
 
 
     }
 
     public static void init(){
             Server server = new Server();
-            //server.letConnect();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("wanna connect?(y/n)");
+        String temp = sc.nextLine();
+        if(temp.equals("y")){
+            System.out.println("IP>>>");
+            String ip = sc.nextLine();
+            System.out.println("Port>>>");
+            int port = sc.nextInt();
+            connect(ip,port);
+        }
+        System.out.println("wanna let others connect?(y/n)");
+        String temp1 = sc.nextLine();
+        if(temp1.equals("y")){
+
+            server.letConnect();
+        }
 
 
 
